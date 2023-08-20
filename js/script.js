a1bad5b8fe918d5304c4a526af4559365cf27719
KolodziejczykTomasz <tomaszkolodziejczyk@wp.pl>
2023-08-20 15:34:17 +0200

    Add new section.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,28 @@
-window.addEventListener('scroll', function (event) {
-    event.preventDefault();
+<script>
+   var macy = Macy({
+  container: '.gallery-wrapper',
+  mobileFirst: true,
+  columns: 1,
+  breakAt: {
+    400: 1,
+    700: 2,
+    1100: 3,
+  },
+  margin: {
+    x: 20,
+    y: 20,
+  }
+})
 
-    if (window.scrollY <= 50) {
-      nav.style.backgroundColor = 'transparent';
-    } else {
-      nav.style.backgroundColor = '#fff';
-      nav.style.opacity = '0.9';
-      nav.style.transition = '0.4s ease';
-    }
+
+</script>
+<script>
+    function toogleShowGallery() {
+    const gallery = document.getElementById("gallery");
+    const shadow = document.getElementById("gallery-background");
+    const btnMore = document.getElementById("button-more")
+    gallery.classList.toggle("show-gallery");
+    shadow.classList.toggle("shadow-gallery-hide");
+    btnMore.classList.toggle("button-white")
+    } 
+</script>
